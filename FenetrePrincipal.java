3978a6ff854037d12b665f4129b9ad2dfcf3b2b6
Héloïse Monnerot-Dumaine <heloise.monnerotdumaine@gmail.com>
2025-05-23 15:01:27 +0200

    Retour à un commit précédent

--- FenetrePrincipal.java
+++ FenetrePrincipal.java
@@ -585,6 +585,7 @@ public class FenetrePrincipal extends JFrame implements Serializable {
             if (mode != null) {
                 partie.setMode(mode);
                 partie.setToursDef(modeDialog.getTours());
+                partie.setDefenseur(modeDialog.getDefenseur());
             }
             else {
                 partie.setMode("annihilation");
@@ -705,7 +706,7 @@ public class FenetrePrincipal extends JFrame implements Serializable {
         String gagnant;
         String message;
         
-        if (partie.getMode() == "annihilation") {
+        if (partie.getMode().equals("annihilation")) {
             boolean joueur1Gagne = partie.getJoueur1().aDesUnitesVivantes();
             
             if (partie.getJoueur2().isEstIA()) {
